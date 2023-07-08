8fb9c643d87974f2b03944955154615bb6283ecd
Pratik-Mahajan257 <79212698+Pratik-Mahajan257@users.noreply.github.com>
2023-07-08 19:21:28 +0530

    checkout page done- UI DONE

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import Header from './Header';
-import { AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Checkout = ({ cartItems, setCartItems }) => {
-
   const [searchQuery, setSearchQuery] = useState('');
 
   const handleRemoveItem = (item) => {
@@ -44,8 +43,8 @@ const Checkout = ({ cartItems, setCartItems }) => {
   const calculateDiscount = () => {
     let discount = 0;
 
-    // Offer 1: Buy 6 cans of Coca-Cola, and get one free
-    const cokeItem = cartItems.find(item => item.name === 'Coca-Cola');
+    // Offer 1: Buy 6 cans of Coca-Cola and get one free
+    const cokeItem = cartItems.find((item) => item.name === 'Coca-Cola');
     if (cokeItem) {
       const cokeItemCount = cokeItem.quantity;
       const cokeOfferCount = Math.floor(cokeItemCount / 6);
@@ -53,8 +52,8 @@ const Checkout = ({ cartItems, setCartItems }) => {
     }
 
     // Offer 2: Buy 3 croissants and get a free coffee
-    const croissantItem = cartItems.find(item => item.name === 'Croissant');
-    const coffeeItem = cartItems.find(item => item.name === 'Coffee');
+    const croissantItem = cartItems.find((item) => item.name === 'Croissant');
+    const coffeeItem = cartItems.find((item) => item.name === 'Coffee');
     if (croissantItem && coffeeItem) {
       const croissantCount = croissantItem.quantity;
       const coffeeOfferCount = Math.floor(croissantCount / 3);
@@ -76,8 +75,13 @@ const Checkout = ({ cartItems, setCartItems }) => {
         {cartItems.length > 0 ? (
           <div className='relative'>
             {cartItems.map((item) => (
-              <div key={item.id} className='bg-white flex-col p-5 relative h-[120px] lg:ml-10 mb-10 lg:w-2/3  lg:h-[100px] border border-gray-200 flex gap-x-10 rounded-2xl shadow-lg'>
-                <h2 className='font-bold text-xl w-fit absolute left-1/3 lg:left-[20%]  lg:top-1/4     '>{item.name}</h2>
+              <div
+                key={item.id}
+                className='bg-white flex-col p-5 relative h-[120px] lg:ml-10 mb-10 lg:w-2/3  lg:h-[100px] border border-gray-200 flex gap-x-10 rounded-2xl shadow-lg'
+              >
+                <h2 className='font-bold text-xl w-fit absolute left-1/3 lg:left-[20%]  lg:top-1/4     '>
+                  {item.name}
+                </h2>
                 <div className='absolute h-[90px] w-[90px] lg:h-[70px]  lg:w-[70px] top-4  '>
                   <img
                     src={item.img}
@@ -85,7 +89,9 @@ const Checkout = ({ cartItems, setCartItems }) => {
                     className='lg:w-full w-80 h-full lg:ml-6 rounded-md object-cover lg:max-h-fit lg:object-cover'
                   />
                 </div>
-                <p className='absolute bottom-1/3 lg:right-0 lg:flex lg:justify-center lg:top-8      left-2/3 '>{item.price}</p>
+                <p className='absolute bottom-1/3 lg:right-0 lg:flex lg:justify-center lg:top-8      left-2/3 '>
+                  {item.price}
+                </p>
                 {item.available >= 10 ? (
                   <p></p>
                 ) : (
@@ -116,25 +122,23 @@ const Checkout = ({ cartItems, setCartItems }) => {
                 </button>
               </div>
             ))}
-            <div className=' flex flex-col justify-center relative items-center'>
-            <div className=' border-t-2 border-gray-300 py-8 w-full flex justify-center items-center '>
-             <p className='font-bold absolute left-1/3   '>Subtotal </p>
-             <p className='absolute left-2/3 '>{calculateSubtotal()}</p>
+            <div className='flex flex-col justify-center relative items-center'>
+              <div className='border-t-2 border-gray-300 py-8 w-full flex justify-center items-center '>
+                <p className='font-bold absolute left-1/3'>Subtotal </p>
+                <p className='absolute left-2/3'>{calculateSubtotal()}</p>
+              </div>
+              <div className='border-t-2 border-gray-300 py-8 w-full flex justify-center items-center '>
+                <p className='font-bold absolute left-1/3'>Discount </p>
+                <p className='absolute left-2/3'>{calculateDiscount()}</p>
+              </div>
+              <div className='border-y-2 border-gray-300 py-8 w-full flex gap-x-7 justify-center items-center '>
+                <p className='font-bold absolute left-1/3'>Total </p>
+                <p className='absolute left-2/3'>{calculateTotal()}</p>
+                <button className='bg-green-500 text-white px-3 py-1 absolute right-0 rounded flex justify-end items-end'>
+                  Checkout
+                </button>
+              </div>
             </div>
-            <div className=' border-t-2 border-gray-300 py-8 w-full flex justify-center items-center '>
-            <p className='font-bold absolute left-1/3  '>Discount </p>
-            <p className='absolute left-2/3'>{calculateDiscount()}</p>
-            </div>
-            <div className=' border-y-2 border-gray-300 py-8 w-full flex gap-x-7 justify-center items-center '>
-            <p className='font-bold absolute left-1/3 '>Total </p>
-             <p className='absolute left-2/3'>{calculateTotal()}</p>
-              <button className='bg-green-500 text-white px-3 py-1 absolute right-0 rounded flex justify-end items-end '>Checkout</button>
-            </div>
-            
-        
-             
-            </div>
-          
           </div>
         ) : (
           <div>No items in the cart.</div>
